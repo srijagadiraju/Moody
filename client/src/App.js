ec1c33bd45af0ef51ec038078036d8dd5378d29a
srijagadiraju <srijasgadiraju@gmail.com>
2024-10-30 16:23:44 -0700

    added mood page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SignUpLoginPage from "./pages/SignUpLoginPage";
+import MoodPage from "./pages/MoodPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<SignUpLoginPage />} />
+          <Route path="/mood-selection" element={<MoodPage />} />
         </Routes>
       </div>
     </Router>
