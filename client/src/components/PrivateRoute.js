bbac80a4842708c595725a785b9c34a4a8e1db11
srijagadiraju <srijasgadiraju@gmail.com>
2024-12-10 13:34:59 -0800

    updated route

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -32,16 +32,61 @@
 
 // export default PrivateRoute;
 
+// import React, { useEffect } from "react";
+// import { Navigate } from "react-router-dom";
+// import { useAuth } from "../AuthContext";
+
+// const PrivateRoute = ({ children }) => {
+//   const { isAuthenticated, loading } = useAuth();
+
+//   useEffect(() => {
+//     if (!isAuthenticated && !loading) {
+//       console.log("User is not authenticated. Redirecting to login page.");
+//     }
+//   }, [isAuthenticated, loading]);
+
+//   console.log("Loading:", loading);
+//   console.log("Is Authenticated:", isAuthenticated);
+
+//   if (loading) return <div>Loading...</div>;
+
+//   return isAuthenticated ? children : <Navigate to="/" replace />;
+// };
+
+// export default PrivateRoute;
+
 import React, { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../AuthContext";
+
+const backendUrl = "https://moody-backend.onrender.com";
 
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated, loading } = useAuth();
 
   useEffect(() => {
     if (!isAuthenticated && !loading) {
-      console.log("User is not authenticated. Redirecting to login page.");
+      console.log(
+        "User has logged out and is being redirected to the login page."
+      );
+
+      // Attempt a backend verification of logout
+      fetch(`${backendUrl}/api/check-auth`, {
+        method: "GET",
+        credentials: "include",
+      })
+        .then((response) => {
+          if (!response.ok) {
+            console.log("Backend indicates user is not authenticated.");
+          } else {
+            console.log(
+              "Backend indicates user is still authenticated. This might be an error."
+            );
+          }
+        })
+        .catch((error) => {
+          console.log("Error connecting to backend for auth check:", error);
+        });
     }
   }, [isAuthenticated, loading]);
 
